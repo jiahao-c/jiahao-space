f65c5e2e95e4df969c1b894f94f0f37f9f4181b4
Jiahao <jiahao.chen@mail.mcgill.ca>
2022-02-20 17:10:07 -0500

    remove unused dependency

--- docs/learnings/courses-elective.md
+++ docs/learnings/courses-elective.md
@@ -2,7 +2,7 @@
 sidebar_position: 4
 ---
 
-# WIP: Fun electives
+# WIP: Fun electives at McGill
 My reflection and comments on some interesting elective courses I took at McGill university.
 (To be written)
 
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,8 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+// const lightCodeTheme = require('prism-react-renderer/themes/github');
+// const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -69,10 +69,10 @@ const config = {
         style: 'dark',
         copyright: `Copyright © ${new Date().getFullYear()}, Built with Docusaurus.`,
       },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
+      // prism: {
+      //   theme: lightCodeTheme,
+      //   darkTheme: darkCodeTheme,
+      // },
       colorMode: {
         defaultMode: 'light',
         disableSwitch: true,
