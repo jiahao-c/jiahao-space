adfdea74d0ef99dc21ed85ba5525e8288b8c834c
Jiahao <jiahao.chen@mail.mcgill.ca>
2022-04-21 21:00:16 -0400

    try babel plugin

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  presets: [require.resolve('@docusaurus/core/lib/babel/preset')],
+  presets: [require.resolve("@docusaurus/core/lib/babel/preset")],
+  plugins: ["transform-inline-environment-variables"],
 };
--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,17 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-// import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 
-require("dotenv").config();
 export const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_ANON_KEY
 );
-
-// const {
-//   siteConfig: { customFields },
-// } = useDocusaurusContext();
-
-// export const supabase = createClient(
-//   customFields.supabaseUrl,
-//   customFields.supabaseAnonKey
-// );
