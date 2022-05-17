d812bec80f75a07f2d5ee7aefed6c6811e73b59f
Jiahao <8275280+jiahao-c@users.noreply.github.com>
2022-05-17 10:49:47 -0400

    add http response

--- api/keep-supabase-alive.ts
+++ api/keep-supabase-alive.ts
@@ -2,16 +2,20 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-    process.env.SUPABASE_URL,
-    process.env.SUPABASE_ANON_KEY
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
 );
 
 export default async (request: VercelRequest, response: VercelResponse) => {
-    let { data, error } = await supabase
+  let { data, error } = await supabase
     .from("project")
     .select("*");
-  if (error) console.error("error", error);
+  if (error) {
+    console.error("error", error);
+    response.status(500).send("something happened");
+  }
   else {
     console.info(data)
+    response.status(200).send("done");
   }
 };
