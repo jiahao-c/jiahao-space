9b032e3bdb56293e5fa16edde1e7acfb262bea93
Jiahao <8275280+jiahao-c@users.noreply.github.com>
2023-05-14 16:59:21 -0700

    Fix title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,9 +62,9 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Jiahao',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Logo',
           src: 'img/logo.png',
         },
         items: [
